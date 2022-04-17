295aac75e57e2d651c988f79ab9771b9a7ae22b2
Lovemore Daka <harrymackinson@hotmail.com>
2022-04-17 11:02:31 +0200

    added service

--- Core/Services/ProcurementPlanDataService.cs
+++ Core/Services/ProcurementPlanDataService.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Models;
 
 namespace Core.Services
 {
     public class ProcurementPlanDataService : IProcurementPlanDataService
     {
-
-
+        public Task<string> SaveAll(List<ProcurementPlanItem> procurementPlanItems)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
