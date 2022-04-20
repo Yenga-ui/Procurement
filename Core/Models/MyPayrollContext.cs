fa6e7bde857284685180d3d0a584a2f691ceb6ec
donsApp <johnbandaofficial@gmail.com>
2022-04-20 01:30:52 +0100

    changed connection string

--- Core/Models/MyPayrollContext.cs
+++ Core/Models/MyPayrollContext.cs
@@ -138,7 +138,7 @@ namespace Core.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=HQ-IPM-CBUM03;Database=MyPayroll;persist security info=True;user id=sa;password=Srax200plus;;");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-KT7SICT\\SQLEXPRESS;Initial Catalog=MyPayroll;Integrated Security=True;");
             }
         }
 
